44d8ce75cfe507d77ac0c4609e42e3c1fa6b4c6e
parzsy <sophie.strong1768@gmail.com>
2026-09-02 17:00:40 -0400

    Refactor log search functionality to improve user interaction and remove unnecessary parameters

--- main.py
+++ main.py
@@ -12,11 +12,23 @@ with open("logs.txt", "r") as file:
 
 counts = {"INFO": 0, "WARNING": 0, "ERROR": 0}
 
+def menu():
+    print("1. Search logs with keyword")
+    print("2. Search logs by level")
+    print("3. Print counts")
+    print("4. Quit")
+    choice = int(input("Enter choice: "))
+    return choice
+
 for log in logs: 
     level = log["level"] 
     counts[level] +=1
 
-def search_logs(keyword):
+def count():
+    print(counts)
+
+def search_logs():
+    keyword = input("Enter keyword: ")
     keyword = keyword.lower()
     results = []
     for log in logs:
@@ -25,16 +37,29 @@ def search_logs(keyword):
             print(f"[{log['level']}] {log['message']}")
     return results
     
-def search_logs_by_level(keyword2):
+def search_logs_by_level():
+    keyword2 = input("Enter level: ")
     keyword2 = keyword2.lower()
     results2 = []
     for log in logs:
         if keyword2 == log["level"].lower():
             results2.append(log)
-            print(f"[{log['level']}] {log['message']}]")
+            print(f"[{log['level']}] {log['message']}")
     return results2
 
-search_logs_by_level("INFO")
+while True:
+    choice = menu()
+    if choice == 1:
+        search_logs()
+    elif choice == 2:
+        search_logs_by_level()
+    elif choice == 3: 
+        count()
+    elif choice == 4:
+        break
+
+
+
 
 
 
