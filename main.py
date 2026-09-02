5d2cba594aaca5630a11be599ad8aad1a3dacc85
parzsy <sophie.strong1768@gmail.com>
2026-09-01 20:49:02 -0400

    Refactor log search functionality to remove hardcoded keyword and update search invocation

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ for log in logs:
     level = log["level"] 
     counts[level] +=1
 
-keyword = "User"
-
 def search_logs(keyword):
     keyword = keyword.lower()
     results = []
@@ -26,8 +24,17 @@ def search_logs(keyword):
             results.append(log)
             print(f"[{log['level']}] {log['message']}")
     return results
+    
+def search_logs_by_level(keyword2):
+    keyword2 = keyword2.lower()
+    results2 = []
+    for log in logs:
+        if keyword2 == log["level"].lower():
+            results2.append(log)
+            print(f"[{log['level']}] {log['message']}]")
+    return results2
 
-search_logs("User")
+search_logs_by_level("INFO")
 
 
 
